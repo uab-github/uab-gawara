4be160eafbbc774531360ae030d419b9cc09d159
myathtut-zafir <aa.htut@gmail.com>
2020-10-20 21:07:50 +0630

    fix join group

--- components/home/Modal.vue
+++ components/home/Modal.vue
@@ -6,7 +6,7 @@
       <div class="modal-dialog" role="document">
         <div class="modal-content">
           <div class="modal-header">
-            <h5 class="modal-title">Group Save</h5>
+            <h5 class="modal-title">Join Group</h5>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
               <span aria-hidden="true" @click="cancel">&times;</span>
             </button>
